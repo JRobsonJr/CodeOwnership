85483fce7a3efea7258f3ed75282b91f3962d12f
mariana-mendes <mariana.silva@ccc.ufcg.edu.br>
2018-09-13 22:41:24 -0300

    Metodos comuns entre a classe abstrata e a filha

--- src/main/java/extractor/Extractor.java
+++ src/main/java/extractor/Extractor.java
@@ -2,6 +2,9 @@ package extractor;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -13,15 +16,18 @@ import com.github.javaparser.ast.type.ClassOrInterfaceType;
 import git.GitRepository;
 
 public class Extractor {
-	
+
 	public static final String LS = System.lineSeparator();
 	private GitRepository repository;
 	private ComponentClass<Object> componentClass;
-	
-	
+	private Map<String, String> extendedType;
+	private Map<String, List<String>> methodsFromClass;
+
 	public Extractor(GitRepository repository) {
 		this.repository = repository;
 		this.componentClass = new ComponentClass<>();
+		this.extendedType = new HashMap<>();
+		this.methodsFromClass = new HashMap<>();
 	}
 
 	public void get() throws ParseException, IOException {
@@ -29,35 +35,51 @@ public class Extractor {
 		this.printMethods();
 		this.printExtendTypes();
 	}
-	
+
 	public void printMethods() {
-			Set<Object> methods = this.componentClass.getMethods().keySet();
-			String line = "";
-			for (Object key : methods) {
-				line = LS + "CLASS: "+ key + LS;
-				line += LS +  "METHODS: " + LS;
-				for (MethodDeclaration method : this.componentClass.getMethods().get(key)) {
-					line += " --------------- " +  method.getDeclarationAsString() + LS;
-				}
-				System.out.println(line);
+		Set<Object> methods = this.componentClass.getMethods().keySet();
+		for (Object key : methods) {
+			this.methodsFromClass.put((String) key.toString(), new ArrayList<>());
+			for (MethodDeclaration method : this.componentClass.getMethods().get(key)) {
+				this.methodsFromClass.get(key).add(method.getDeclarationAsString(false, false, false));
+
 			}
+		}
 	}
-	
+
 	public void printExtendTypes() {
 		Map<Object, NodeList<ClassOrInterfaceType>> classes = this.componentClass.getInheritance();
 		Set<Object> c = classes.keySet();
 		for (Object object : c) {
-			if(classes.get(object).size() > 0) {
-				System.out.print("CLASS: " + object + "  ");
-				System.out.println("EXTENDS: " + classes.get(object));
+			if (classes.get(object).size() > 0) {
+				this.extendedType.put((String) object,
+						classes.get(object).toString().substring(1, classes.get(object).toString().length() - 1));
+			}
+		}
+		printCommonMethods();
+	}
+
+	public void printCommonMethods() {
+		Set<String> classes = this.extendedType.keySet();
+		for (String string : classes) {
+			System.out.print(LS + "CLASS>>>" + string);
+			System.out.println("   EXTENDED>>>" + this.extendedType.get(string));
+
+			List<String> superMethods = this.methodsFromClass.get(this.extendedType.get(string));
+
+			List<String> methods = this.methodsFromClass.get(string);
+
+			System.out.println("Common methods beetwen " + string + " and " + this.extendedType.get(string));
+
+			for (String superMethod : superMethods) {
+				for (String method : methods) {
+					if (superMethod.equals(method)) {
+						System.out.println(method);
+					}
+
+				}
+
 			}
 		}
 	}
-	
-	public void printCommonMethods() {
-		
-	}
-	
-
-	
 }
